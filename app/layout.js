02fa9d200fbdbd7beae87139b5d0b3cb86cac35a
laserine32 <laserinobatbatuk32@gmail.com>
2025-01-02 19:21:07 +0700

    fixing pagination production & change revalidate to 0

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css"
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants"
 import { ThemeProvider } from "next-themes"
 
-export const revalidate = 60
+export const revalidate = 0
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
